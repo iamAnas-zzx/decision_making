238a8608cc9b36e6eec5d764f78ef87edf79bf2d
Ahmad Anas <102130991+iamAnas-zzx@users.noreply.github.com>
2024-05-09 15:19:18 +0530

    fixed venv

--- decision_making/min_dependency.py
+++ decision_making/min_dependency.py
@@ -1,7 +1,7 @@
 # All minimum dependencies for decsion-making
 NUMPY_MIN_VERSION = "1.26.4"
 SCIPY_MIN_VERSION = "1.13.0"
-PANDA_MIN_VERSION = "2.2.1"
+PANDA_MIN_VERSION = "2.2.2"
 
 
 MIN_DEPENDENCIES = {
